45b276c424bd451e5f3e0827fcc9ac7d597d581c
Mitchell <me@mreiff.dev>
2023-04-22 17:03:54 -0400

    userView page now has an admin edit button

--- routes/users.js
+++ routes/users.js
@@ -1,40 +1,38 @@
-import {Router} from 'express';
+import { Router } from "express";
 const router = Router();
-import {ticketData} from '../data/index.js';
-import {userData} from '../data/index.js';
-import * as helpers from "../helpers.js"; 
+import { ticketData } from "../data/index.js";
+import { userData } from "../data/index.js";
+import * as helpers from "../helpers.js";
 
 router
-  .route('/')
+  .route("/")
   .get(async (req, res) => {
     try {
-    let users = await userData.getAll();
+      let users = await userData.getAll();
 
-    res.status(200).render("allUsersView", {
-      title: "Users View",
-      tickets: users
-    });
-
-  }catch(e) {
-    res.status(404).render("404", {
-      title: "404 Tickets not found",
-      msg: "Error 404: Issue Retrieving tickets"});
-  }
+      res.status(200).render("allUsersView", {
+        title: "Users View",
+        tickets: users,
+      });
+    } catch (e) {
+      res.status(404).render("404", {
+        title: "404 Tickets not found",
+        msg: "Error 404: Issue Retrieving tickets",
+      });
+    }
     //code here for GET
   })
   .post(async (req, res) => {
     //code here for POST
-   
   });
 
-  router
-  .route('/view/:id')
-  .get(async (req, res) => {
-    try {
-      let user = await userData.get(req.params.id);
-      const name = `${user.firstName} ${user.lastName}`
+router.route("/view/:id").get(async (req, res) => {
+  try {
+    let user = await userData.get(req.params.id);
+    const name = `${user.firstName} ${user.lastName}`;
+    if (req.session.user.role.toLowerCase() === "admin") {
       res.status(200).render("userView", {
-        title: name,
+        id: user._id,
         name: name,
         username: user.username,
         email: user.email,
@@ -42,16 +40,31 @@ router
         title: user.title,
         createdTickets: await ticketData.getMultiple(user.createdTickets),
         ownedTickets: await ticketData.getMultiple(user.ticketsBeingWorkedOn),
-        commentsLeft: user.commentsLeft
+        commentsLeft: user.commentsLeft,
+        admin: true,
       });
-    } catch (e) {
-      console.log(e);
-      res.status(404).render("404", {
-        title: "404 User not found",
-        msg: "Error 404: User ID Not Found"});
+    } else {
+      res.status(200).render("userView", {
+        id: user._id,
+        name: name,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        title: user.title,
+        createdTickets: await ticketData.getMultiple(user.createdTickets),
+        ownedTickets: await ticketData.getMultiple(user.ticketsBeingWorkedOn),
+        commentsLeft: user.commentsLeft,
+      });
     }
-    
-    //code here for GET
-  });
+  } catch (e) {
+    console.log(e);
+    res.status(404).render("404", {
+      title: "404 User not found",
+      msg: "Error 404: User ID Not Found",
+    });
+  }
 
-  export default router;
+  //code here for GET
+});
+
+export default router;
