605f9f43add56bbff780f8d94fc233e2ca641f97
Mitchell <me@mreiff.dev>
2023-04-22 12:37:23 -0400

    Basic register form and /register route from lab 10

--- routes/main.js
+++ routes/main.js
@@ -92,4 +92,74 @@ router.route("/logout").get(
     res.redirect("/");
   }
 );
+
+router
+  .route("/register")
+  .get(
+    (req, res, next) => {
+      if (req.session.user) {
+        return res.redirect("/");
+      } else {
+        req.method = "GET";
+        next();
+      }
+    },
+    async (req, res) => {
+      //code here for GET
+      try {
+        res.status(200).render("register", { title: "Register" });
+      } catch (e) {
+        res.status(500).render("error", {
+          title: "Error",
+          error: "internal server error",
+          code: "500",
+        });
+      }
+    }
+  )
+  .post(async (req, res) => {
+    //code here for POST
+    // console.log(req.body);
+    try {
+      if (
+        req.body.hasOwnProperty("firstNameInput") &&
+        req.body.hasOwnProperty("lastNameInput") &&
+        req.body.hasOwnProperty("emailAddressInput") &&
+        req.body.hasOwnProperty("passwordInput") &&
+        req.body.hasOwnProperty("confirmPasswordInput") &&
+        req.body.hasOwnProperty("roleInput")
+      ) {
+        let firstName = checkName(req.body["firstNameInput"], "first name");
+        let lastName = checkName(req.body["lastNameInput"], "last name");
+        let emailAddress = checkEmail(req.body["emailAddressInput"]);
+        let password = checkPassword(req.body["passwordInput"]);
+        if (password != req.body["confirmPasswordInput"]) {
+          throw new Error("your passwords do not match");
+        }
+        let role = checkRole(req.body["roleInput"]);
+
+        let user = await createUser(
+          firstName,
+          lastName,
+          emailAddress,
+          password,
+          role
+        );
+
+        if (
+          user.hasOwnProperty("insertedUser") &&
+          user["insertedUser"] == true
+        ) {
+          // res.status(200).render("login", { title: "Login" });
+          res.status(200).redirect("/login");
+        } else {
+          throw new Error("unable to create user");
+        }
+      }
+    } catch (e) {
+      // render form with 400 code
+      res.status(400).render("register", { title: "Register", error: `${e}` });
+    }
+  });
+
 export default router;
