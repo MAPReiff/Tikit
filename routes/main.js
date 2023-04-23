7ad1fea6ac79196a0e36d82c5564cc4547631e4b
Mitchell <me@mreiff.dev>
2023-04-22 21:12:33 -0400

    Login page bootstrap

--- routes/main.js
+++ routes/main.js
@@ -45,7 +45,7 @@ router
     async (req, res) => {
       //code here for GET
       try {
-        res.status(200).render("login", { title: "Login" });
+        res.status(200).render("login", { title: "Login", loginPage: true });
       } catch (e) {
         res.status(500).render("error", {
           title: "Error",
