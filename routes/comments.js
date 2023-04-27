793e03c7ff79788ab265825e8120e533522a1568
David Bajollari <dbajollari1@gmail.com>
2023-04-26 21:17:44 -0400

    form now allows one to add comment

--- routes/comments.js
+++ routes/comments.js
@@ -5,13 +5,20 @@ import {ticketData} from '../data/index.js';
 import {userData} from '../data/index.js';
 import {commentData} from '../data/index.js';
 import * as helpers from "../helpers.js"; 
-import { renderError } from '../helpers.js';
+import { renderError, renderError400 } from '../helpers.js';
 
 
 router
   .route('/:ticketId')
   /* get all comments under given ticketId*/
-  .get(async (req, res) => {
+  .get(
+    (req, res, next) => {
+      if (!req.session.user) {
+        return res.redirect('/login');
+      }
+      next();
+    },
+    async (req, res) => {
     try {
       req.params.ticketId = helpers.checkId(req.params.ticketId, 'ID URL Param');
     } catch (e) {
@@ -21,8 +28,8 @@ router
     try {
       const curTicket = await ticketData.get(req.params.ticketId);
       const comments = await commentData.getAll(req.params.ticketId);
-      if (comments.length === 0) throw "No comments found with that ticket id"
-      res.json(comments);
+      if (comments.length === 0) throw "No comments found with that ticket id";
+      res.status(200).render("commentView", {ticketId: req.params.ticketId} );
     } catch (e) {
       renderError(res, 404, 'Resource not found');
     }
@@ -30,43 +37,26 @@ router
   })
   /*add comment to given ticket, need to pass userId in body as well as content, may be able to change how this works later*/
   .post(async (req, res) => {
+    let commentInfo = req.body;
     try {
       req.params.ticketId = helpers.checkId(req.params.ticketId, 'ID URL Param');
+      if (!commentInfo || Object.keys(commentInfo).length === 0) throw 'There are no fields in the request body'
+      if ( !commentInfo.contentInput) throw 'Not all neccessary fields provided in request body'; 
+      commentInfo.contentInput = helpers.checkString(commentInfo.contentInput, 'content');
     } catch (e) {
-      return res.status(400).json({error: e});
-    }
-
-    let commentInfo = req.body;
-    if (!commentInfo || Object.keys(commentInfo).length === 0) {
-      return res
-        .status(400)
-        .json({error: 'There are no fields in the request body'});
-    }
-
-    if (!commentInfo.userId || !commentInfo.content) {
-      return res
-        .status(400)
-        .json({error: 'Not all neccessary fields provided in request body'});
-    }
-    try {
-      commentInfo.userId = helpers.checkId(commentInfo.userId, 'User ID');
-      commentInfo.content = helpers.checkString(commentInfo.content, 'content');
-    } catch (e) {
-      return res.status(400).json({error: e});
+      renderError400(res, 400, 'Missing request body info');
     }
 
     try {
       let curTicket = await ticketData.get(req.params.ticketId); 
-      let curUser = await userData.get(commentInfo.userId); 
     } catch (e) { 
-      return res.status(404).json({error: e});
+      renderError(res, 404, 'Resource not found');
     }
 
     try {
-      const {userId, content} = commentInfo;
-      const newComment = await commentData.create(req.params.ticketId, userId, content);
-      let updatedTicket = await ticketData.get(req.params.ticketId); 
-      res.json(updatedTicket);
+      const newComment = await commentData.create(req.params.ticketId, req.session.user._id, 0, commentInfo.contentInput);
+      let redirectURL = '/tickets/view/' +  req.params.ticketId; 
+      res.redirect(redirectURL); 
     } catch (e) {
       res.status(500).json({error: e});
     }
