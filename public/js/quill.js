ebbd387d9d784d6bbb90e6e587cd5e5d0efec19a
Mitchell <me@mreiff.dev>
2023-05-02 22:51:51 -0400

    Rich text in comments & replies

---
+++ public/js/quill.js
@@ -0,0 +1,34 @@
+// rich text editor
+Quill.register("modules/imageCompressor", imageCompressor);
+var quill = new Quill("#editor", {
+  modules: {
+    toolbar: [
+      [{ size: ["small", false, "large", "huge"] }],
+      ["bold", "italic", "underline", "strike"],
+      ["image"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["clean"],
+    ],
+    imageCompressor: {
+      quality: 0.7,
+      maxWidth: 500, // default
+      maxHeight: 500, // default
+      imageType: 'image/jpeg'
+    }
+  },
+  theme: "snow",
+});
+
+// get the hidden input - from https://stackoverflow.com/a/67682117
+let inputElement = document.getElementById("contentInput");
+quill.on("text-change", function () {
+  // console.log(quill.getText())
+  let richText = quill.getText().trim().replaceAll(/\s+/g, "");
+  // console.log(richText.length)
+  if (richText.length !== 0 || richText === "\n") {
+    // inputElement.value = JSON.stringify(quill.getContents());
+    inputElement.value = quill.root.innerHTML;
+  } else {
+    inputElement.value = "";
+  }
+});
